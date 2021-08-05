9bcf3546c03955321835caddf040c58207c584ad
googlemarketer <trustrajeshg@gmail.com>
2021-08-05 15:23:42 +0530

    initital commit

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -11,7 +11,7 @@ class ContactServiceProvider extends ServiceProvider
     {
 
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/views', 'contact');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'contact');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         //$this->mergeConfigFrom(__DIR__.'/config/contact.php', 'contact');
         $this->publishes([__DIR__.'/config/contact.php' => config_path('contact.php')]);
